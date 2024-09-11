5294a95348eba8f9242a81c532a6fc614f2c203f
github <githubbs7>
2024-09-11 23:59:49 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './App.css'
+
+
+function App() {
+  const [color, setColor] = useState("white")
+  return (
+      <>
+      
+          <div className="h-screen w-100" style={{backgroundColor: color}}></div>
+          <div className="bg">
+              <button style={{background: "red"}} onClick={() =>{
+                  setColor("red")
+              }}>Red</button>
+              <button style={{background: "green"}} onClick={() =>{
+                  setColor("green")
+              }}>Green</button>
+              <button style={{background: "Blue"}} onClick={() =>{
+                  setColor("blue")
+              }}>Blue</button>
+              <button style={{background: "#ff2b9d"}} onClick={() =>{
+                  setColor("white")
+              }}>Reset</button>
+              
+          </div>
+      </>
+  )
+}
+
+export default App;
